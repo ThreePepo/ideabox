ec74c6df2265d68f7fb341d487497d341cbd3579
Warren Pung <swpnik@gmail.com>
2012-11-30 09:46:01 +0800

    BaseService基础类

--- ideabox-server/src/com/taobao/ideabox/service/BaseService.java
+++ ideabox-server/src/com/taobao/ideabox/service/BaseService.java
@@ -5,7 +5,6 @@ package com.taobao.ideabox.service;
  * User: Warren
  * Date: 12-11-28
  * Time: ионГ11:02
- * To change this template use File | Settings | File Templates.
  */
 public interface BaseService {
     public void test();
